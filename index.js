a0149337a59b7e36b0f99ea17c141137a72fcab7
Arjen <124262203+arjenxyz@users.noreply.github.com>
2023-02-23 14:02:45 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+// join our discord discord.gg/uoaio or dm me Uo#1428 if you need any type of help
+
+
+const http = require("http");
+http.createServer((_, res) => res.end("İtalyan 31")).listen(8080)
+
+require(`./src/index`);
+
