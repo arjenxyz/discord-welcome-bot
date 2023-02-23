f4dde916f0794609bdb150365e1f0c0f40ef40a9
Arjen <124262203+arjenxyz@users.noreply.github.com>
2023-02-23 14:16:03 +0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-// join our discord discord.gg/uoaio or dm me Uo#1428 if you need any type of help
-
-
-const http = require("http");
-http.createServer((_, res) => res.end("İtalyan 31")).listen(8080)
-
-require(`./src/index`);
-
