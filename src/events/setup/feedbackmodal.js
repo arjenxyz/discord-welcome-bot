767c2d228d66c847dbc5b238010eb0001b4a4335
Arjen <124262203+arjenxyz@users.noreply.github.com>
2023-02-23 14:19:33 +0300

    Add files via upload

---
+++ src/events/setup/feedbackmodal.js
@@ -0,0 +1,28 @@
+const client = require("../../index")
+const server = require("../../config/server.json");
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = {
+    name: "feedbackmodalLoaded"
+};
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isModalSubmit()) return;
+    if (interaction.customId === 'feedbackmodal') {
+        await interaction.reply({ content: 'Sorununuz Yetkililere İletildi. Lütfen bekleyiniz..', ephemeral: true });
+    }
+    const channel = interaction.guild.channels.cache.get(`${server.channel.feedback}`)
+    const feedback = interaction.fields.getTextInputValue('feedback');
+
+    const embed = new EmbedBuilder()
+        .setAuthor({ name: `${interaction.user.username} adlı kullanıcı tarafından gönderildi`, iconURL: interaction.user.displayAvatarURL() })
+        .setDescription(`**Hey! Sanırım bir sorunumuz var** : \n${feedback}`)
+    
+    try {
+        channel.send({
+            embeds: [embed]
+        })
+    } catch (err) {
+        console.log(err)
+    }
+});
