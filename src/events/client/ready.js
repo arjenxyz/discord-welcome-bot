7f2a67bcde8dbb8051381555d8e0771e3b492c4b
Arjen <124262203+arjenxyz@users.noreply.github.com>
2023-02-24 14:36:30 +0300

    Update ready.js

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -9,11 +9,13 @@ module.exports = {
 client.once('ready', async () => {
     const users = client.users.cache.size
 
-    console.log(`[READY] ${client.user.tag} ÇALIŞIOM AMK NE BAKION `.bold)
+    console.log(`Ben ${client.user.tag},\n
+İtalyan#0142 Tarafından oluşturuldum.\n
+Gizem Kurt'un Yan Çarları sunucusunda aktif olarak hizmet veriyorum.`.bold)
     console.log("----------------------------------------".white);
 
     setInterval(() => {
-        const statuses = [`Made By İtalyan | Gizem Kurt'un Yan Çarları`];
+        const statuses = [`Made By İtalyan`];
         const status = statuses[Math.floor(Math.random() * statuses.length)];
         client.user.setActivity(status, { type: ActivityType.Watching });
     }, 60000);
