80ce7e2166ba35f5180b4e828e62bc90d04fec7d
Arjen <124262203+arjenxyz@users.noreply.github.com>
2023-02-24 15:10:27 +0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-// join our discord discord.gg/gizemkurt or dm me İtalyan#0142 if you need any type of help
-
-
-const http = require("http");
-http.createServer((_, res) => res.end("Made By İtalyan")).listen(8080)
-
-require(`./src/index`);
-
